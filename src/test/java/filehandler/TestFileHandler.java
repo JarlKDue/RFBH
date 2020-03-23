0092160fff13609af71939640cbd6cb418e9745a
jkd <jkd@miracle.dk>
2020-03-23 15:55:21 +0100

    Adding Test Case

---
+++ src/test/java/filehandler/TestFileHandler.java
@@ -0,0 +1,13 @@
+package filehandler;
+
+import org.junit.jupiter.api.Test;
+
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapters;
+
+public class TestFileHandler {
+
+    @Test
+    public void test_this(){
+        System.out.println("HEllo This");
+    }
+}
